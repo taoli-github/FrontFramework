01c06d181a7d35d12e944a2b3042d4e30a8b70ed
taoli-github <lt594610341@hotmail.com>
2018-02-23 17:01:21 +0800

    商品管理

---
+++ js/mini.js
@@ -0,0 +1,34 @@
+//#region 获取url中的参数
+function GetRequest(key) {
+    var url = location.search.toLowerCase(); //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest[key.toLowerCase()];
+}
+
+// 获取good info
+function getGoodInfo(id,server_adress)
+{
+    var re_data = "";
+    var v_url = "http://" + server_adress + ":5000/api/good";
+    $.ajax({
+        url: v_url + "?ID=" + id,
+        type:"GET",
+        async:false,
+        success:function(data,textStatus,jqXhr){
+            //console.log(data);
+            re_data = eval("(" + data + ")");
+        },
+        error:function(err){
+            alert(err);
+        }
+    });
+
+    return re_data;
+}
